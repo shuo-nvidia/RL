16843df7e5f4f11c7c50aabd686c25ca22287282
Zhaopeng Qiu <alexq@nvidia.com>
2025-09-28 19:43:59 -0700

    revert cp param
    Signed-off-by: Zhaopeng Qiu <alexq@nvidia.com>

--- nemo_rl/models/policy/dtensor_policy_worker.py
+++ nemo_rl/models/policy/dtensor_policy_worker.py
@@ -807,9 +807,6 @@ class DTensorPolicyWorker:
                             mb,
                             global_valid_seqs,
                             global_valid_toks,
-                            context_parallel_group=self.cp_mesh.get_group()
-                            if self.cp_size > 1
-                            else None,
                         )
                         del logits
 
